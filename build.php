4189c45e281032a2f25c716897374930f7aff9af
Guillaume Pasquet <guillaume@tutorfair.com>
2013-07-30 09:58:08 +0100

    Made phars executable.

--- build.php
+++ build.php
@@ -3,5 +3,6 @@
 // create with alias "project.phar"
 $phar = new Phar('centrifuge.phar', 0, 'centrifuge.phar');
 $phar->buildFromDirectory(__DIR__ . '/src');
-$phar->setStub($phar->createDefaultStub('centrifuge.php', 'centrifuge.php'));
+$stub = $phar->createDefaultStub('centrifuge.php', 'centrifuge.php');
+$phar->setStub("#!/usr/bin/env php\n" . $stub);
  
